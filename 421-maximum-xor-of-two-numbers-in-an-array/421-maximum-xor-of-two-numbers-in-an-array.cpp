ff40b40b649e23cd47cf9c31c40d64005f917941
Deeksha <deekshasharma2022@gmail.com>
2022-02-13 09:54:13 +0530

    Time: 750 ms (29.85%), Space: 74.5 MB (43.44%) - LeetHub

---
+++ 421-maximum-xor-of-two-numbers-in-an-array/421-maximum-xor-of-two-numbers-in-an-array.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int findMaximumXOR(vector<int>& nums) {
+        //This variable will store the final ans of this question
+        int maxXor = 0;
+        //This will be storing the prefixes 
+        unordered_set<int> prefixes;
+        
+        //Traversing the 32 bits of an integer
+        for(int i = 31; i >= 0; i--){
+            //mask for this bit
+            int mask = (mask|(1<<i));
+            int maxPossible = (maxXor|(1 << i));
+            //Traversing the array elements
+            for(int j = 0; j < nums.size(); j++){
+                //Inserting the prefixes in the set
+                prefixes.insert(nums[j]&mask);
+            }
+            
+            //Now using the concept a^b = c then a^c= b
+            for(auto i: prefixes){
+                if(prefixes.find(maxPossible^i) != prefixes.end()){
+                    //Update the maximum one
+                    maxXor |= maxPossible;
+                    break;
+                }
+            }
+            //clear the prefixes set
+            prefixes.clear();
+        }
+        return maxXor;
+    }
+};
