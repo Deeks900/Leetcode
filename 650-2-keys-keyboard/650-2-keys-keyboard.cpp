14c778f19d63d746f349128e705965ce4501ef14
Deeksha <deekshasharma2022@gmail.com>
2022-03-04 16:26:20 +0530

    Time: 0 ms (100.00%), Space: 6 MB (64.83%) - LeetHub

--- 650-2-keys-keyboard/650-2-keys-keyboard.cpp
+++ 650-2-keys-keyboard/650-2-keys-keyboard.cpp
@@ -1,27 +1,15 @@
 class Solution {
 public:
-    int keysHelper(int n, int countAScreen, int copyStatus, int pasteCount){
-        //Base Case
-        if(countAScreen > n){
-            return 10001;
-        }
-        
-        if(countAScreen == n){
-            return 0;
-        }
-    
-        //Recursive Calls
-        if(copyStatus == 0){
-            return 1+keysHelper(n, countAScreen, 1, countAScreen);
-        }
-            int op1 = 1+keysHelper(n, countAScreen+pasteCount, copyStatus, pasteCount);
-            int op2 = 1+keysHelper(n, countAScreen+pasteCount, 0, 0);
-            return min(op1, op2);
-       
-        
-    }
-    
     int minSteps(int n) {
-        return keysHelper(n, 1, 0, 0);
+        int ans = 0;
+        int d = 2;
+        while(n > 1){
+            while(n%d == 0){
+                ans += d;
+                n /= d;
+            }
+            d++;
+        }
+        return ans;
     }
 };
