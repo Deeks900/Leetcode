54a8337b986164bd2571fc038e59042e04dff683
Deeksha <deekshasharma2022@gmail.com>
2022-04-27 16:07:10 +0530

    Time: 43 ms (70.92%), Space: 20 MB (27.40%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -5,16 +5,11 @@ public:
         vector<vector<int>> mergedAns;
         
         for(auto i: intervals){
-            if(mergedAns.size() == 0){
+            if(mergedAns.size() == 0 || mergedAns.back()[1] < i[0]){
                mergedAns.push_back(i); 
             }
             else{
-                if(mergedAns.back()[1] >= i[0]){
-                    mergedAns[mergedAns.size()-1][1] = max( mergedAns[mergedAns.size()-1][1], i[1]);
-                }
-                else{
-                     mergedAns.push_back(i);
-                }
+                  mergedAns.back()[1] = max(mergedAns.back()[1], i[1]);
             }
         }
         return mergedAns;
